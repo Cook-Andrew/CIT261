77b40f180fada7742fed6931c50eba02038d2c73
Andrew Cook <endercook@byui.edu>
2020-03-24 22:30:17 -0600

    quiz Json

--- app/quiz.js
+++ app/quiz.js
@@ -2,11 +2,21 @@ $(document).ready(function () {
     $("#quiz").load("quiz.html", function(){
         $("#check").hide();
         $("#wrong").hide();
-        $("#button").click(function(){
+        $("#completion").attr("max", quizJson.answers.length);
+        $("#question").text(quizJson.questions[index]);
+        $("input:first").on("focusout",function() {
+            /* $("#button").trigger("click");  //turns out that this double triggers if you click the button
+        });
+        $("#button").click(function(){ */
             var answer = $("input:first").val();
-            if (answer == "answer") {
+            if (answer.toUpperCase() == quizJson.answers[index].toUpperCase()) {
+                $("input:first").val("");
                 $("#check").show().animate({fontSize: "15rem"}, 600).animate({ fontSize: "3rem"},300).queue(function(next){
                     $(this).hide();
+                    if (index < quizJson.answers.length) {
+                        index += 1;
+                    }
+                    $("#question").text(quizJson.questions[index]);
                     next();
                 });
             } else {
